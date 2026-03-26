945647a2ad2806ff4256a9bbf382595e7ee84227
Alex Luciano <alex.luciano.topan@gmail.com>
2026-03-25 21:47:11 -0300

    feat: implementando o Identity no Blazor

--- Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
+++ Dima.Api/obj/Debug/net8.0/Dima.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca4bde98ef29f2a6ce77bc47801e937bb61944c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ed415815bee169d12d12c63af1b9f0710c2d1fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dima.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

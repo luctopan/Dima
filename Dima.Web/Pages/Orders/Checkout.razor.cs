6ca18d98a7ff6afa953cc56b77e46c936ae761b2
Alex Luciano <alex.luciano.topan@gmail.com>
2026-09-03 21:22:59 -0300

    Feat: implementado a página de Planos e iniciando a tela de checkout

---
+++ Dima.Web/Pages/Orders/Checkout.razor.cs
@@ -0,0 +1,46 @@
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Orders;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Orders;
+
+public partial class CheckoutPage : ComponentBase
+{
+    #region Parameters
+    
+    [Parameter]
+    public string ProductSlug { get; set; } = string.Empty;
+    
+    [SupplyParameterFromQuery(Name = "voucher")]
+    public string? VoucherNumber { get; set; }
+
+    #endregion
+
+    #region Properties
+
+    public bool IsBusy { get; set; }
+    public bool IsValid { get; set; }
+    public CreateOrderRequest InputModel { get; set; } = new();
+    public Product? Product { get; set; }
+    public Voucher? Voucher { get; set; }
+    public decimal Total { get; set; }
+    
+    #endregion
+    
+    #region Services
+    
+    [Inject]
+    public IProductHandler ProductHandler { get; set; } = null!;
+    [Inject]
+    public IOrderHandler OrderHandler { get; set; } = null!;
+    [Inject]
+    public IVoucherHandler VoucherHandler { get; set; } = null!;
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+}

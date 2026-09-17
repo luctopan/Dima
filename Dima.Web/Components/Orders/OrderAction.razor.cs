640382e6c4e72d4383ee13c186f7557e6c65ddfd
Alex Luciano <alex.luciano.topan@gmail.com>
2026-09-16 21:30:00 -0300

    Feat: implementando a página de detalhes do pedido.

---
+++ Dima.Web/Components/Orders/OrderAction.razor.cs
@@ -0,0 +1,14 @@
+using Dima.Core.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Dima.Web.Components.Orders;
+
+public partial class OrderActionComponent : ComponentBase
+{
+    #region Parameters
+
+    [Parameter, EditorRequired]
+    public Order Order { get; set; } = null!;
+
+    #endregion
+}
